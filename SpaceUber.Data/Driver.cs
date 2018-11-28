2f5a287cb6d251ac88fe24240f70288d086f70f6
caldwellusmc <deshanecaldwell@yahoo.com>
2018-11-28 16:43:57 -0500

    3rd commit

---
+++ SpaceUber.Data/Driver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceUber.Data
+{
+    public class Driver
+    {
+        [Key]
+        public int DriverId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public int LicenseNumber { get; set; }
+
+        [Required]
+        public string SpaceshipMake { get; set; }
+
+        public int ReviewId { get; set; }
+    }
+}
---
+++ SpaceUber.Data/Rider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceUber.Data
+{
+    public class Rider
+    {
+        public int RiderId { get; set; }
+        public Guid OwnerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Destination { get; set; }
+        public int ReviewId { get; set; }
+    }
+}
