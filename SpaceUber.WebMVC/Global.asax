c980c00c62f4f5f58e64181b2f0e91e72b572de6
caldwellusmc <deshanecaldwell@yahoo.com>
2018-11-28 11:12:43 -0500

    Add project files.

---
+++ SpaceUber.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaceUber.WebMVC.MvcApplication" Language="C#" %>
---
+++ SpaceUber.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaceUber.WebMVC.Startup))]
+namespace SpaceUber.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
