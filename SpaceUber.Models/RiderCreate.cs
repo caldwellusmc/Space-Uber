7608ac368af7b4a0da189713ed3f5ee8268de067
caldwellusmc <deshanecaldwell@yahoo.com>
2018-12-10 16:33:08 -0500

    39th commit

--- SpaceUber.Models/RiderCreate.cs
+++ SpaceUber.Models/RiderCreate.cs
@@ -11,14 +11,17 @@ namespace SpaceUber.Models
     {
         [Required]
         [MinLength(1, ErrorMessage = "Please enter your first name.")]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
 
         [Required]
         [MinLength(1, ErrorMessage = "Please enter your last name")]
+        [Display(Name ="Last Name")]
         public string LastName { get; set; }
 
         [Required]
         [MinLength(1, ErrorMessage ="Please enter a destination.")]
+        [Display(Name ="Destination")]
         public string Destination { get; set; }
 
         public override string ToString() => FirstName;
